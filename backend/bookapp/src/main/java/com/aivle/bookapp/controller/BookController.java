843bbd5b19fc48cf86a225ef90d1aea4ac670f16
thadus2 <152844617+thadus2@users.noreply.github.com>
2026-06-09 15:14:53 +0900

    api 매핑 중복 개선

--- backend/bookapp/src/main/java/com/aivle/bookapp/controller/BookController.java
+++ backend/bookapp/src/main/java/com/aivle/bookapp/controller/BookController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/localhost:8080/api/v1")
+@RequestMapping("/api/v1/books")
 @RequiredArgsConstructor
 public class BookController {
 
@@ -19,21 +19,21 @@ public class BookController {
 
 // 전체 조회 + 검색
 
-    @GetMapping("/books")
+    @GetMapping("")
     public ResponseEntity<List<Book>> getBooks(@RequestParam(required = false) String keyword) {
         return ResponseEntity.ok(bookService.getBooks(keyword));
     }
 
 // 단건 조회
 
-    @GetMapping("/books/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Book> getBook(@PathVariable Long id) {
         return ResponseEntity.ok(bookService.findById(id));
     }
 
 // 등록
 
-    @PostMapping("/books")
+    @PostMapping("")
     public ResponseEntity<Book> createBook(@Valid @RequestBody Book book) {
         Book saved = bookService.create(book);
 
@@ -42,7 +42,7 @@ public class BookController {
 
 // 수정
 
-    @PatchMapping("/books/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book book) {
         Book updated = bookService.update(id, book);
 
@@ -51,7 +51,7 @@ public class BookController {
 
 // 삭제
 
-    @DeleteMapping("/books/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
         bookService.deleteBook(id);
 
